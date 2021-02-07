e6a995696b74afa91f79cf6ee83d8ab8cd9b2041
maaami98 <35247648+maaami98@users.noreply.github.com>
2021-02-07 20:43:08 +0300

    Update 2.flash.bat

--- 2.flash.bat
+++ 2.flash.bat
@@ -1,4 +1,4 @@
 set COMPORT=COM3
 python -m esptool --port %COMPORT% erase_flash
-python -m esptool --port %COMPORT% --chip esp8266 write_flash 0x00000 "esp8266-20191220-v1.12.bin"
+python -m esptool --port %COMPORT% --chip esp8266 write_flash --flash_size=detect -fm dio 0 "esp8266-20191220-v1.12.bin"
 pause
